98958c58ea1312123d7b1c4cd6a7833af44c03ab
keon820-maker <keon820@gmail.com>
2026-09-03 12:51:30 +0900

    v0.11: bump version for spatial OCR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "kr.co.investigation.manager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.10.0"
+        versionCode = 11
+        versionName = "0.11.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
