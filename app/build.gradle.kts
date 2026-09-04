fe20f9697c32670076719eb77484615ef2a8b74c
keon820-maker <keon820@gmail.com>
2026-09-04 11:13:02 +0900

    version 0.23.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "kr.co.investigation.manager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.22.0"
+        versionCode = 23
+        versionName = "0.23.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
