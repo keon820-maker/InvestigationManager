1da6fdd540e41abe79b014749cf7eb6e0585d0ff
keon820-maker <keon820@gmail.com>
2026-09-04 09:49:19 +0900

    Bump app version to 0.21.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "kr.co.investigation.manager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.20.0"
+        versionCode = 21
+        versionName = "0.21.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
