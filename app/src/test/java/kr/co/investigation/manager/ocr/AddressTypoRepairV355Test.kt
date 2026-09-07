ba3643a2771daa26c028b084e584a3d79c1b8ffe
keon820-maker <keon820@gmail.com>
2026-09-07 13:08:50 +0900

    Add address token regressions from latest device batch

--- app/src/test/java/kr/co/investigation/manager/ocr/AddressTypoRepairV355Test.kt
+++ app/src/test/java/kr/co/investigation/manager/ocr/AddressTypoRepairV355Test.kt
@@ -14,6 +14,14 @@ class AddressTypoRepairV355Test {
             "00000 서울특별시 테스트구 테스트로 1 (송파동 테스트아파트)",
             AddressTypoRepairV355.normalize("00000 서울특별시 테스트구 테스트로 1 (송피파동 테스트아파트)")
         )
+        assertEquals(
+            "00000 경기도 테스트시 테스트길 2-7 주원하우스 4층",
+            AddressTypoRepairV355.normalize("00000 경기도 테스트시 테스트길 2-7 주원히우스 4층")
+        )
+        assertEquals(
+            "00000 경기도 테스트시 테스트로 43 센트럴 푸르지오 118동 604호",
+            AddressTypoRepairV355.normalize("00000 경기도 테스트시 테스트로 43 센트럴 푸르지오오피트 118동 604호")
+        )
     }
 
     @Test
