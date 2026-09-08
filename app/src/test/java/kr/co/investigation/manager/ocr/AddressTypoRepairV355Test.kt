5c072cb3ce55e57b346c6c5be3ae9eef3c75ad76
keon820-maker <keon820@gmail.com>
2026-09-08 13:17:55 +0900

    Fix six digit prefix test expectations

--- app/src/test/java/kr/co/investigation/manager/ocr/AddressTypoRepairV355Test.kt
+++ app/src/test/java/kr/co/investigation/manager/ocr/AddressTypoRepairV355Test.kt
@@ -31,11 +31,11 @@ class AddressTypoRepairV355Test {
             AddressTypoRepairV355.normalize("12791 경기도 광주시 테스트로 1")
         )
         assertEquals(
-            "경기도 성남시 분당구 테스트로 2",
+            "경기 성남시 분당구 테스트로 2",
             AddressTypoRepairV355.normalize("463400 경기 성남시 분당구 테스트로 2")
         )
         assertEquals(
-            "경기도 성남시 수정구 테스트로 3",
+            "경기 성남시 수정구 테스트로 3",
             AddressTypoRepairV355.normalize("461160 경기 성남시 수정구 테스트로 3")
         )
     }
