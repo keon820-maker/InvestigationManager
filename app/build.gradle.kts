592c8869b9cbd5db484eb7301ceb6e24152ddfea
keon820-maker <keon820@gmail.com>
2026-09-08 13:15:02 +0900

    Bump version to 0.35.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "kr.co.investigation.manager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 58
-        versionName = "0.35.23"
+        versionCode = 59
+        versionName = "0.35.24"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
