c8cc2a6647c30c2d70a312f878daacf2194724ff
keon820-maker <keon820@gmail.com>
2026-09-20 13:41:46 +0900

    Release v0.36.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "kr.co.investigation.manager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 69
-        versionName = "0.35.34"
+        versionCode = 70
+        versionName = "0.36.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
